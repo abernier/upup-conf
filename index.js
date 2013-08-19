35aaca43931ec7aaa5c882c258cd3fdd75a7cd6e
Antoine BERNIER <antoine.bernier@gmail.com>
2013-08-19 19:16:35 +0200

    path.resolve

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 var ini = require('ini');
 
-var defaults = ini.parse(fs.readFileSync('.upuprc-dist', 'utf-8'));
+var defaults = ini.parse(fs.readFileSync(path.resolve(__dirname, '.upuprc-dist'), 'utf-8'));
 
 module.exports = require('rc')('upup', defaults);
